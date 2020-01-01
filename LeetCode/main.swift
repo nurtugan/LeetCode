283117f3810e570feef1e7411ae75b18b985a685
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-01-01 22:53:24 +0600

    Initial Commit

---
+++ LeetCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by Nurtugan Nuraly on 1/1/20.
+//  Copyright © 2020 XFamily. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
