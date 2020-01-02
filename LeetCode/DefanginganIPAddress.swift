93c522855a603b19b4fa49dfba516f650c83705c
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-01-02 18:25:54 +0600

    Defanging an IP Address

---
+++ LeetCode/DefanginganIPAddress.swift
@@ -0,0 +1,15 @@
+//
+//  DefanginganIPAddress.swift
+//  LeetCode
+//
+//  Created by Nurtugan Nuraly on 1/2/20.
+//  Copyright © 2020 XFamily. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+  func defangIPaddr(_ address: String) -> String {
+    address.replacingOccurrences(of: ".", with: "[.]")
+  }
+}
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -10,5 +10,5 @@ import Foundation
 
 let solution = Solution()
 
-let result = solution.twoSum([2,7,11,15], 9)
+let result = solution.defangIPaddr("1.1.1.1")
 print(result)
